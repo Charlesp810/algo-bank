ef1faeba495893f11cb48b17f1ea552f14d45347
Charlies Pan <Cp81089@gmail.com>
2020-11-19 15:37:22 -0500

    getNthFib

--- index.js
+++ index.js
@@ -150,3 +150,14 @@ class Node {
     return array
   }
 }
+
+/*getNthFib Time: O(n) Space: O(n) */
+function getNthFib(n) {
+  let memo = { 1: 0, 2: 1 }
+  if (n in memo) {
+    return memo[n]
+  } else {
+    memo[n] = getNthFib(n - 1, memo) + getNthFib(n - 2, memo)
+    return memo[n]
+  }
+}
