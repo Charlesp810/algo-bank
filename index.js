0735b53bd45e7cb8b7e11adda0d3480f5c695d7f
Charlies Pan <Cp81089@gmail.com>
2020-12-16 20:27:14 -0500

    spiralTraverse

--- index.js
+++ index.js
@@ -358,3 +358,39 @@ function isMonotonic(array) {
     return true
   }
 }
+
+/*spiralTraverse Space: O(n) Time: O(n) where n is the number of elems */
+function spiralTraverse(array) {
+  let result = []
+  let startRow = 0
+  let startCol = 0
+  let endRow = array.length - 1
+  let endCol = array[0].length - 1
+
+  while (startRow <= endRow && startCol <= endCol) {
+    for (let col = startCol; col <= endCol; col++) {
+      result.push(array[startRow][col])
+    }
+
+    for (let row = startRow + 1; row <= endRow; row++) {
+      result.push(array[row][endCol])
+    }
+
+    for (let col = endCol - 1; col >= startCol; col--) {
+      if (startRow === endRow) break;
+      result.push(array[endRow][col])
+    }
+
+    for (let row = endRow - 1; row > startRow; row--) {
+      if (startCol === endCol) break;
+      result.push(array[row][startCol])
+    }
+
+    startRow++
+    startCol++
+    endRow--
+    endCol--
+  }
+
+  return result
+}
