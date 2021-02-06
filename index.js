42e6a9e79849871bf2c074e8522620c1d9bbff23
Charlies Pan <Cp81089@gmail.com>
2021-02-05 19:12:51 -0500

    insertionSort

--- index.js
+++ index.js
@@ -447,3 +447,17 @@ function removeDuplicatesFromLinkedList(linkedList) {
   }
   return linkedList;
 }
+
+/*insertionSort Time: O(n^2) Space: O(1) */
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i
+    while (j > 0 && array[j] < array[j - 1]) {
+      let temp = array[j]
+      array[j] = array[j - 1]
+      array[j - 1] = temp
+      j--
+    }
+  }
+  return array
+}
