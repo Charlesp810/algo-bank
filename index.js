a3316ec89c2e3f399aab28802685a6682c1b36b8
Charlies Pan <Cp81089@gmail.com>
2021-02-18 19:20:30 -0500

    minHeightBst with and without insert method

--- index.js
+++ index.js
@@ -682,3 +682,68 @@ function traverse(tree, min, max) {
   if (tree.value < min || tree.value >= max) return false;
   return traverse(tree.left, min, tree.value) && traverse(tree.right, tree.value, max)
 }
+
+/*minHeightBst (with insert method) Time: O(nlog(n)) Space: O(n) */
+function minHeightBst(array) {
+  return constructMinBst(array, null, 0, array.length - 1)
+}
+
+function constructMinBst(array, bst, startIdx, endIdx) {
+  if (endIdx < startIdx) return
+  let midIdx = Math.floor((startIdx + endIdx) / 2)
+  let addValue = array[midIdx]
+  if (bst === null) {
+    bst = new BST(addValue)
+  } else {
+    bst.insert(addValue)
+  }
+  constructMinBst(array, bst, startIdx, midIdx - 1)
+  constructMinBst(array, bst, midIdx + 1, endIdx)
+  return bst
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+}
+
+/*minHeightBst (WITHOUT insert method) Time: O(n) Space: O(n) */
+function minHeightBst(array) {
+  return constructMinBst(array, 0, array.length - 1)
+}
+
+function constructMinBst(array, startIdx, endIdx) {
+  if (endIdx < startIdx) return null;
+  let midIdx = Math.floor((startIdx + endIdx) / 2);
+  const bst = new BST(array[midIdx]);
+  bst.left = constructMinBst(array, startIdx, midIdx - 1)
+  bst.right = constructMinBst(array, midIdx + 1, endIdx)
+  return bst
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
